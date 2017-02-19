ecef35ab0b19226a0aa0c7a292b4e2383abd7c7a
Payyanz <abicabraham@gmail.com>
2017-02-19 18:47:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,11 +5,85 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articles = {
+    'article-1':{
+         title: 'Payyanz | first article',
+         heading: 'My first article page',
+         date: '19/02/2017',
+         content: `<p> My name Abi and I am testing My first article page here.
+                    "I hereby submit the bill for structural painting of pipeline (Applying two coats of Epoxy coltar over one coat of Zinc Phosphate primer) carried out at BPCL Irimpanam Installation for an amount of Rs. 38,462.50/-(Rupees Thirty eight thousand four hundred sixty two and Paisa fifty only).The total weight of structural painted is 9.05 MT (measurement sheet attached) @ Rs.4250 per MT." 
+                    </p>
+                    <p> My name Abi and I am testing My first article page here."I hereby submit the bill for structural painting of pipeline (Applying two coats of Epoxy coltar over one coat of Zinc Phosphate primer) carried out at BPCL Irimpanam Installation for an amount of Rs. 38,462.50/-(Rupees Thirty eight thousand four hundred sixty two and Paisa fifty only).The total weight of structural painted is 9.05 MT (measurement sheet attached) @ Rs.4250 per MT."
+                    </p>
+                    <p> My name Abi and I am testing My first article page here."I hereby submit the bill for structural painting of pipeline (Applying two coats of Epoxy coltar over one coat of Zinc Phosphate primer) carried out at BPCL Irimpanam Installation for an amount of Rs. 38,462.50/-(Rupees Thirty eight thousand four hundred sixty two and Paisa fifty only).The total weight of structural painted is 9.05 MT (measurement sheet attached) @ Rs.4250 per MT."
+                    </p>`
+    },
+    'article-2':{
+     title: 'Payyanz | second article',
+     heading: 'My second article page',
+     date: '20/02/2017',
+     content: `<p> My name Abi and I am testing My first article page here.
+                "I hereby submit the bill for structural painting of pipeline (Applying two coats of Epoxy coltar over one coat of Zinc Phosphate primer) carried out at BPCL Irimpanam Installation for an amount of Rs. 38,462.50/-(Rupees Thirty eight thousand four hundred sixty two and Paisa fifty only).The total weight of structural painted is 9.05 MT (measurement sheet attached) @ Rs.4250 per MT." 
+                </p>
+                <p> My name Abi and I am testing My first article page here."I hereby submit the bill for structural painting of pipeline (Applying two coats of Epoxy coltar over one coat of Zinc Phosphate primer) carried out at BPCL Irimpanam Installation for an amount of Rs. 38,462.50/-(Rupees Thirty eight thousand four hundred sixty two and Paisa fifty only).The total weight of structural painted is 9.05 MT (measurement sheet attached) @ Rs.4250 per MT."
+                </p>
+                <p> My name Abi and I am testing My first article page here."I hereby submit the bill for structural painting of pipeline (Applying two coats of Epoxy coltar over one coat of Zinc Phosphate primer) carried out at BPCL Irimpanam Installation for an amount of Rs. 38,462.50/-(Rupees Thirty eight thousand four hundred sixty two and Paisa fifty only).The total weight of structural painted is 9.05 MT (measurement sheet attached) @ Rs.4250 per MT."
+                </p>`
+    },
+    'article31':{
+         title: 'Payyanz | third article',
+         heading: 'My third article page',
+         date: '21/02/2017',
+         content: `<p> My name Abi and I am testing My first article page here.
+                    "I hereby submit the bill for structural painting of pipeline (Applying two coats of Epoxy coltar over one coat of Zinc Phosphate primer) carried out at BPCL Irimpanam Installation for an amount of Rs. 38,462.50/-(Rupees Thirty eight thousand four hundred sixty two and Paisa fifty only).The total weight of structural painted is 9.05 MT (measurement sheet attached) @ Rs.4250 per MT." 
+                    </p>
+                    <p> My name Abi and I am testing My first article page here."I hereby submit the bill for structural painting of pipeline (Applying two coats of Epoxy coltar over one coat of Zinc Phosphate primer) carried out at BPCL Irimpanam Installation for an amount of Rs. 38,462.50/-(Rupees Thirty eight thousand four hundred sixty two and Paisa fifty only).The total weight of structural painted is 9.05 MT (measurement sheet attached) @ Rs.4250 per MT."
+                    </p>
+                    <p> My name Abi and I am testing My first article page here."I hereby submit the bill for structural painting of pipeline (Applying two coats of Epoxy coltar over one coat of Zinc Phosphate primer) carried out at BPCL Irimpanam Installation for an amount of Rs. 38,462.50/-(Rupees Thirty eight thousand four hundred sixty two and Paisa fifty only).The total weight of structural painted is 9.05 MT (measurement sheet attached) @ Rs.4250 per MT."
+                    </p>`
+    },
+};               
+
+
+function createtemplate(data){
+    var title = data.title;
+    var heading = data.heading;
+    var date = data.date;
+    var content = data.content;
+    var htmltemplate = `
+    <html>
+    <head>
+        <title> 
+        ${title}
+        </title>
+            <meta name="viewport" content="width=device-width,initial-scale=1"/>
+            <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div class="container">
+            <div>
+                <a href='/'>HOME</a>
+            </div>
+            <div>
+             ${heading}
+            </div>
+            <div>
+            ${date}
+            </div>
+            <div>
+                ${content}
+            </div>
+    </body>
+</html>`;
+}
+
+
         app.get('/', function (req, res) {
           res.sendFile(path.join(__dirname, 'ui', 'index.html'));
         });
-        app.get('/article-1', function (req, res) {
-        res.sendFile(path.join(__dirname, 'ui', 'article one.html'));
+        var articlename=req.params.articlename;
+        app.get('/:article name', function (req, res) {
+        res.send(path.join(createtemplate(artcles[articlename])));
         });
         app.get('/article-2', function (req, res) {
           res.send('My article two');
