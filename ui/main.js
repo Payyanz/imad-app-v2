b9d84860450e6cecf9be736491e35720168c457a
Payyanz <abicabraham@gmail.com>
2017-02-20 20:16:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,10 @@
 console.log('Loaded!');
 var element = document.getElementById('main-text')
 ;
-element.innerHTML = "My test run"
+element.innerHTML = "My test run";
+
+var img = document.getElementByID('madi');
+
+img.onclick = function(){
+    var interval = setInterval(moveLeft,1);
+};
